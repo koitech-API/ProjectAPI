552f45df3628b3725948254e95ef55755850cab3
João Luís <joaoluis14.carvalho@gmail.com>
2025-09-20 14:14:47 -0300

    feat: Adiciona Sobre e Header

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host="0.0.0.0")
