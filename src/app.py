ba13541ba9319c96b621ab1958ee4c65bf93c0ca
guilherme <guilherme.alvarenga021@gmail.com>
2025-10-21 12:56:26 -0300

    feat: integrando no site os gráficos interativos feitos no colab utilizando plotly

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    #caminho até os arquivos dos gráficos
+    razao_jovens_ocupacao_2010 = os.path.join("static", "assets", "plots", "razao_jovens_ocupacao_2010.json")
+    distribuicao_renda_2010 = os.path.join("static", "assets", "plots", "distribuicao_renda_2010.json")
+
+    #abre e carrega os arquivos dos gráficos
+    with open(razao_jovens_ocupacao_2010, "r", encoding="utf-8") as f:
+        razao_jovens_ocupacao_2010_json = json.load(f)
+    with open(distribuicao_renda_2010, "r", encoding="utf-8") as o:
+        distribuicao_renda_2010_json = json.load(o)
+
+    return render_template(
+        "index.html",
+        razao_jovens_ocupacao_2010_json=razao_jovens_ocupacao_2010_json,
+        distribuicao_renda_2010_json=distribuicao_renda_2010_json
+    )
 
 @app.route("/sobre")
 def sobre():
