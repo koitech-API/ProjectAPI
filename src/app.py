15ed79f7d14f0d509a58db00070cf18b83c1eb77
Giovana <152290946+giotrzz@users.noreply.github.com>
2025-10-23 17:57:31 -0300

    adicionando gráficos escolaridade 2010
    Added loading for two new JSON graph files: 'grafico_escolaridade_comparacao.json' and 'grafico_escolaridade_detalhado.json'. Updated the return statement to include these new data.

--- src/app.py
+++ src/app.py
@@ -10,16 +10,28 @@ def index():
     razao_jovens_ocupacao_2010 = os.path.join("static", "assets", "plots", "razao_jovens_ocupacao_2010.json")
     distribuicao_renda_2010 = os.path.join("static", "assets", "plots", "distribuicao_renda_2010.json")
 
+    grafico_escolaridade_comparacao = os.path.join("static", "assets", "plots", "grafico_escolaridade_comparacao.json")
+    grafico_escolaridade_detalhado = os.path.join("static", "assets", "plots", "grafico_escolaridade_detalhado.json")
+
     #abre e carrega os arquivos dos gráficos
     with open(razao_jovens_ocupacao_2010, "r", encoding="utf-8") as f:
         razao_jovens_ocupacao_2010_json = json.load(f)
     with open(distribuicao_renda_2010, "r", encoding="utf-8") as o:
         distribuicao_renda_2010_json = json.load(o)
+        
+    with open(grafico_escolaridade_comparacao, "r", encoding="utf-8") as f:
+        grafico_escolaridade_comparacao_json = json.load(f)
+    
+    with open(grafico_escolaridade_detalhado, "r", encoding="utf-8") as f:
+        grafico_escolaridade_detalhado_json = json.load(f)
 
     return render_template(
         "index.html",
         razao_jovens_ocupacao_2010_json=razao_jovens_ocupacao_2010_json,
         distribuicao_renda_2010_json=distribuicao_renda_2010_json
+        
+        grafico_escolaridade_comparacao_json=grafico_escolaridade_comparacao_json,
+        grafico_escolaridade_detalhado_json=grafico_escolaridade_detalhado_json
     )
 
 @app.route("/sobre")
@@ -28,3 +40,5 @@ def sobre():
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host="0.0.0.0")
+
+print("Gráfico salvo e download iniciado!")
